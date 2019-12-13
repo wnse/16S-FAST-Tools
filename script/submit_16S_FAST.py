82e66738363b5ad059a65f534d2b68a1181a7076
wnse <yangkai07@gmail.com>
2019-12-13 10:50:13 +0800

    remove some files

---
+++ script/submit_16S_FAST.py
@@ -0,0 +1,88 @@
+import json
+import os
+import pandas as pd
+import argparse
+import time
+import logging
+                
+def submit_16S_FAST(ar1,ar2,lr1,lr2,remark,name,result_path='/mnt/data/yangkai/16S_FAST/',group=0):
+    result_path = os.path.join(result_path,remark,name)
+    cmd_list = ['/root/anaconda3/bin/python3.7', 
+                '/Bioinfo/16S-FAST-Tools/16S_FAST_Analysis_0.4.1.py', 
+                '-ar1 ${ar1} -ar2 ${ar2} -lr1 ${lr1} -lr2 ${lr2}',
+                '-o', '${outdir}',
+                '-n', name,
+                '-c', str(16), 
+               # '-minl', '1200', 
+               # '-maxl', '1700'
+               ]
+    if group == 1:
+        cmd_list.append('-group')
+    cmd=' '.join(cmd_list)
+    config={ 
+                'application':
+                {
+                    'cmd':cmd,
+                    'id':1,
+                    'name':'16S_FAST_Analysis_0.3.1',
+                },
+                'baseData':
+                {
+                    'remark':remark
+                },
+                'datas':
+                [
+                    {
+                        'objectKey':ar1,'paramKey':'ar1'
+                    },
+                    {
+                        'objectKey':ar2,'paramKey':'ar2'
+                    },
+                    {
+                        'objectKey':lr1,'paramKey':'lr1'
+                    },
+                    {
+                        'objectKey':lr2,'paramKey':'lr2'
+                    },
+                ],
+                'name':name,
+                'result':
+                {
+                    'path':result_path,
+                    'paramKey':'outdir'
+                },
+                'type':'aln'    
+    }
+    config_json=(json.dumps(config))
+    commond="curl -X POST https://bc.dev.germountx.com/api/tasks -H 'Content-Type: application/json' -d " + \
+            "'" + config_json + "'"
+    #logging.info(commond)
+    out=os.popen(commond)
+    logging.info('{} {}'.format(name,out.read()))
+        
+                
+if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s',level=logging.INFO) 
+    date = time.strftime("%Y%m%d%H%M%S", time.localtime()) 
+    __version__ = "0.1_20190918"
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--version', action='version',version='%(prog)s {version}'.format(version=__version__))
+    parser.add_argument('-ar1','--Assemble_Read_1',help='Assemble Read 1 fastq data absolute path in bc-input',required=True)
+    parser.add_argument('-ar2','--Assemble_Read_2',help='Assemble Read 2 fastq data absolute path in bc-input',required=True)
+    parser.add_argument('-lr1','--Linker_Read_1',help='Linker Read 1 fastq data absolute path in bc-input',required=True)
+    parser.add_argument('-lr2','--Linker_Read_2',help='Linker Read 2 fastq data absolute path in bc-input',required=True)
+    
+    parser.add_argument('-n','--name',help='sample name.',required=True)
+    parser.add_argument('-remark','--remark',help='batch name. default:Current Time',default=date)
+    parser.add_argument('-o','--outdir',help='result dir in bc-output. default:/mnt/data/yangkai/16S_FAST/remark/name',\
+                        default='/mnt/data/yangkai/16S_FAST/')
+    args = parser.parse_args() 
+    ar1 = args.Assemble_Read_1
+    ar2 = args.Assemble_Read_2
+    lr1 = args.Linker_Read_1
+    lr2 = args.Linker_Read_2                
+    remark = args.remark
+    name = args.name
+    
+    submit_16S_FAST(ar1,ar2,lr1,lr2,remark,name)
+    
---
+++ script/submit_16S_FAST_group_from_list.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+import os
+import sys
+import time
+import logging
+import argparse
+#pwd = sys.path[0]
+#sys.path.append('pwd')
+import submit_16S_FAST
+
+def submit_16S_FAST_group_from_list(df_tmp,colums_tmp,remark_tmp,name_tmp,group=1):
+    for i in df_tmp.index:
+        sample_name = df_tmp.loc[i,int(name_tmp)-1]
+        submit_16S_FAST.submit_16S_FAST(df_tmp.loc[i,int(colums_tmp[0])-1],\
+                        df_tmp.loc[i,int(colums_tmp[1])-1],\
+                        df_tmp.loc[i,int(colums_tmp[2])-1],\
+                        df_tmp.loc[i,int(colums_tmp[3])-1],\
+                        remark_tmp,\
+                        sample_name, 
+                        group=1)
+        logging.info('{}'.format(sample_name))
+        
+if __name__ == '__main__':
+    date = time.strftime("%Y%m%d%H%M%S", time.localtime()) 
+    logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s',level=logging.INFO) 
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-file','--list_file',help='16S_data_list',required=True)
+    parser.add_argument('-data_col','--col_of_data_path',help='columns of data path (a1,a2,l1,l2). defalut=2,4,6,8',default='2,4,6,8')
+    parser.add_argument('-name_col','--col_of_name',help='columns of sample name. defalut=1',default=1)
+    parser.add_argument('-remark','--remark',help='batch name. default:Current Time',default=date)
+    args = parser.parse_args()
+    
+    df = pd.read_csv(args.list_file,header=None,sep='\t')
+    colums = args.col_of_data_path.split(',')
+    remark = args.remark
+    name = args.col_of_name
+    
+    submit_16S_FAST_group_from_list(df,colums,remark,name)
